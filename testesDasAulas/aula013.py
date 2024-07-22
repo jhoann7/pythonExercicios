f836b4032de3f2e78de6d631501e3a7412fb1766
Jhoann Feitosa <cjhooann@gmail.com>
2024-07-22 19:18:25 -0300

    9

---
+++ testesDasAulas/aula013.py
@@ -0,0 +1,16 @@
+'''for c in range(10, -1, -1):
+    print(c)
+print('FIM')'''
+
+'''i = int(input('Início: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+
+for c in range(i, f, p):
+    print(c)'''
+
+s = 0
+for c in range (0, 4):
+    n = int(input('Digite um valor: '))
+    s = s + n
+print('O somatório de todos os valores é {}'.format(s))
