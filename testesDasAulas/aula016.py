a5e809137512c35344d69df59ff562fe939c34bf
Jhoann Feitosa <cjhooann@gmail.com>
2024-08-01 21:36:30 -0300

    29

---
+++ testesDasAulas/aula016.py
@@ -0,0 +1,21 @@
+# alcool = ('Red label', 'Cavalo branco', '51', 'Corote')
+# print(alcool[:4])
+# for bebida in alcool:
+'''for cont in range(0, len(alcool)):
+    print(f'Eu bebi {alcool[cont]}')
+print('Bebi igual um condenado')'''
+# for pos, bebida in enumerate(alcool):
+#     print(f'Eu bebi {bebida} na posição {pos}')
+# print(sorted(alcool))
+# print(alcool)
+
+# a = (1, 5, 7)
+# b = (2, 0, 0, 4)
+# c = a+b
+# print(c[0])
+# print(c.count(0))
+# print(c)
+# print(c.index(7))
+
+lindao = ('Jhoann', 20, 'M', 1.78)
+print(lindao[0])
